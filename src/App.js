6aa43c5171ba76f3f7a6b863f946fc6da5895b51
Aman <aman13xrestha@gmail.com>
2021-05-29 17:06:27 +0545

    uncontrolled form component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import React, { useState } from "react";
 import colorData from "./color-data.json";
 import './App.css';
 import ColorList from "./ColorList.js";
+import AddColorForm from "./AddColorForm";
 
 function App() {
   console.log(colorData);
   const [colors, setColors] = useState(colorData);
   console.log("Colors");
   console.log(colors);
-  return <ColorList 
-    colors={colors}
-    onRateColor = {
-      (id, rating) => {
-        const newColors = colors.map(color =>
-          color.id === id ? {...color, rating} : color
-        );
-        setColors(newColors)
+  return (
+  <>
+    <AddColorForm />
+
+    <ColorList 
+      colors={colors}
+      onRateColor = {
+        (id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? {...color, rating} : color
+          );
+          setColors(newColors)
+        }
       }
-    }
-    onRemoveColor = { id => {
-      const newColors = colors.filter(color => color.id !== id);
-      setColors(newColors);
-    }} 
-  />
+      onRemoveColor = { id => {
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }} 
+    />
+  </>
+  );
 }
 
 export default App;
